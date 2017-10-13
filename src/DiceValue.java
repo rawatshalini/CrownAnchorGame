cd979a8f7df1f1c96d601f8e26c439302625d4e9
rawatshalini <30823647+rawatshalini@users.noreply.github.com>
2017-10-12 22:05:37 -0700

    //bug 3 - Spade card should be displayed

--- src/DiceValue.java
+++ src/DiceValue.java
@@ -23,6 +23,7 @@ public enum DiceValue {
 	
 	public static DiceValue getRandom() {
 		int random = RANDOM.nextInt(DiceValue.SPADE.ordinal());
+		//bug 3 - Spade card should be displayed
 		return values()[random];
 	}
 	
